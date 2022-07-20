d6e73501f295d73c9aaabe6b0b106caf09108fdc
Abhinav <as3368@live.mdx.ac.uk>
2022-07-20 22:40:59 +0100

    Dash but with keyboard input

--- Assets/Scripts/Knife.cs
+++ Assets/Scripts/Knife.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class Knife : MonoBehaviour
 {
-    public float _upwardsVelocity = 1f;
-    public float _forwardVelocity = 1f;
     [SerializeField]
-    //private Animator _animator;
+    private float _minSwipeDistance;
+    [SerializeField]
+    private float _dashRate;
+    private float _lastDash;
     private Rigidbody rb;
     private GameManager _gameManager;
+    private Vector3 _firstTouchPos;
+    private Vector3 _finalTouchPos;
+    private float _swipeAngle;
+    public float _upwardsVelocity = 1f;
+    public float _forwardVelocity = 1f;
     public float _torque = 1f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-       // _animator = GetComponent<Animator>();
         _gameManager = GameObject.FindObjectOfType<GameManager>();
     }
 
@@ -25,31 +30,68 @@ public class Knife : MonoBehaviour
         {
             if (Input.GetMouseButtonDown(0))
             {
-                rb.velocity = Vector3.up * _upwardsVelocity;
-                //rb.velocity = Vector3.right * _forwardVelocity;
-                rb.AddForce(Vector3.forward * _forwardVelocity * Time.deltaTime, ForceMode.Impulse);
-                //if(_animator.GetBool("Rotate") == false)
-                //{
-                //    _animator.SetBool("Rotate", true);
-
-                //}
-                rb.AddTorque(new Vector3(_torque, 0, 0), ForceMode.Impulse);
+                KnifeMovement();
+                _firstTouchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             }
-            Debug.Log(transform.InverseTransformDirection(rb.angularVelocity).x);
-
-            if(transform.InverseTransformDirection(rb.angularVelocity).x > 0.02)
+            if(Input.GetMouseButtonUp(0))
             {
-                rb.AddTorque(new Vector3(-(_torque/8), 0, 0), ForceMode.Impulse);
+                _finalTouchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                float swipeDist = Mathf.Abs(_finalTouchPos.x - _firstTouchPos.x);
+                //Debug.Log(swipeDist);
+                //CalculateAngle();
             }
+            //Debug.Log(transform.InverseTransformDirection(rb.angularVelocity).x);
+
+            //Stopping the knife from unlimited rotation speed when touch is spammed
+            KnifeAngularDrag();
+            //test
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                rb.angularVelocity = Vector3.zero;
+                KnifeDash();
             }
-            //if(Input.GetMouseButtonUp(0))
+
+            //if (swipeDist > _minSwipeDistance)
             //{
-            //    _animator.SetBool("Rotate", false);
+
+            //    Debug.Log("Swiped");
             //}
         }
 
+    }
+
+    private void KnifeDash()
+    {
+        if (Time.time > _dashRate + _lastDash)
+        {
+            rb.angularVelocity = Vector3.zero;
+            rb.AddForce(Vector3.forward * _forwardVelocity * 2.5f * Time.deltaTime, ForceMode.Impulse);
+            _lastDash = Time.time;
+        }
+    }
+
+    private void KnifeMovement()
+    {
+        rb.velocity = Vector3.up * _upwardsVelocity;
+        rb.AddForce(Vector3.forward * _forwardVelocity * Time.deltaTime, ForceMode.Impulse);
+        rb.AddTorque(new Vector3(_torque, 0, 0), ForceMode.Impulse);
+    }
+    private void KnifeAngularDrag()
+    {
+        if (transform.InverseTransformDirection(rb.angularVelocity).x > 0.02)
+        {
+            rb.AddTorque(new Vector3(-(_torque / 8), 0, 0), ForceMode.Impulse);
+        }
+    }
+    private void CalculateAngle()
+    {
+        _swipeAngle = Mathf.Atan2(_finalTouchPos.y - _firstTouchPos.y, _finalTouchPos.x - _firstTouchPos.x) * 180/Mathf.PI;
+        Debug.Log(_swipeAngle);
+    }
+
+    private void SwipeDistanceCheckMet()
+    {
+        float swipeDist = Mathf.Abs(_finalTouchPos.z - _firstTouchPos.z);
+        Debug.Log(swipeDist);
+        //return swipeDist > _minSwipeDistance;
     }
 }
